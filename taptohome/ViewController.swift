9319d76c7e410f3994c22f69515349952e1477cb
Ryo Ishimoto <iryond@iryond.local>
2018-11-10 21:43:25 +0900

    Initial Commit

---
+++ taptohome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taptohome
+//
+//  Created by Ryo Ishimoto on 2018/11/10.
+//  Copyright © 2018 iryond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ taptohomeTests/taptohomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  taptohomeTests.swift
+//  taptohomeTests
+//
+//  Created by Ryo Ishimoto on 2018/11/10.
+//  Copyright © 2018 iryond. All rights reserved.
+//
+
+import XCTest
+@testable import taptohome
+
+class taptohomeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
